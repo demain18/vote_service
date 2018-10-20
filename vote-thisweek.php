48f6a6c2eed706c9513f6a5dc97517f85b19af36
demain18 <happysugar180@gmail.co>
2018-10-20 23:11:32 +0900

    Prototype 0.0.6
    프론트앤드 거의 작업종료, 남은 페이지들은 지금 있는걸 가져다 붙히기만 하면 됨.  이제 백앤드 개발 필요

--- vote-thisweek.php
+++ vote-thisweek.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
+<html lang="en"><head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic+Coding" rel="stylesheet">
-        <link rel="stylesheet" href="dashboard.css">
+        <link rel="stylesheet" href="vote-thisweek.css">
         <link rel="stylesheet" href="my-navbar.css">
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
         <script>
+            
                 $(document).ready(function () {
                     $('body').bootstrapMaterialDesign();
                 });
@@ -23,6 +23,26 @@
         <body>
             <div class="background-grid1">
                 <div class="background-color01"></div>
+                <!--<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-custom">
+                    <a class="navbar-brand" href="index.html">
+                        <strong>
+                            <span></span>청원 서비스</strong>
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <i class="material-icons">notifications</i>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.html">
+                                    <i class="material-icons">account_circle</i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>-->
                 <div class="my-navbar">
                     <div class="navbar-container">
                         <span class="brand">
@@ -39,12 +59,14 @@
 							<em class="material-icons">account_circle</em>
 						  </button>
 						  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="lr1">
+							  
 							<a href="my-upload.php"><button class="dropdown-item" type="button">내가 올린 게시물</button></a>
 							<a href="setting.php"><button class="dropdown-item" type="button">설정</button></a>
 							<a href="report.php"><button class="dropdown-item" type="button">글 신고하기</button></a>
 							<a href="page-for-admin.php"><button class="dropdown-item disabled" type="button">관리자 모드</button></a>
 							<div class="dropdown-divider"> </div>
 							<a href="contact.php"><a class="dropdown-item" href="#">Contact us</a></a>
+							 
 						  </div>
 						</div>
                         </span>
@@ -52,81 +74,62 @@
                 </div>
                 <!--<span class="school-info row"><p><strong>서울아이티고등학교</strong></p><img src="asset/img/highschool-logo1.png" width="30" height="25"></span>-->
                 <div class="postlist">
-                    <a href="writing.html">
-                        <button type="button" class="btn btn-raised btn-primary btn-write">글 쓰기<em class="material-icons icon-set02">edit</em>
-                        </button>
-                    </a>
-                    <h5 class="board-title">
-                        <strong>최다 추천 청원 목록</strong>
-                    </h5>
                     <div class="post-menus">
                         <ul class="nav nav-pills nav-fill">
                             <li class="nav-item">
                                 <a class="nav-link" href="dashboard.php">전체</a>
                             </li>
-                            <li class="nav-item nav-active">
+                            <li class="nav-item">
                                 <a class="nav-link" href="dashboard.php">최신인기</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="vote-thisweek.php">청원투표</a>
+                            <li class="nav-item nav-active">
+                                <a class="nav-link" href="vote-thisweek.html">이주의 청원</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" href="dashboard.php">청원다시보기</a>
+                                <a class="nav-link" href="re-vote.html">청원다시보기</a>
                             </li>
                         </ul>
                     </div>
-
-                    <p class="menu-list">
-                        <span class="menu menu01">번호</span>
-                        <span class="menu menu02">제목</span>
-                        <span class="menu menu03">청원일</span>
-                        <span class="menu menu04">참여인원</span>
-                    </p>
-                    <div class="post-grid">
-
-					<div class="col-md-6 col-sm-6 accordion-margin">
-						<div class="panel-group wrap" id="accordion" role="tablist" aria-multiselectable="true">
-                          <?php
-                          $servername = "localhost";
-                          $username = "root"; // your username
-                          $password = "1234"; // your password
-                          $conn = mysqli_connect($servername, $username, $password, "voteservice");
-                          
-                          $sql = "SELECT * FROM post ORDER BY post_number DESC";
-                          $result = mysqli_query($conn, $sql)or die(mysqli_error($conn));
-                          while($post = mysqli_fetch_array($result)) {
-                            echo '
-                                <div class="panel">
-                                <div class="panel-heading" role="tab" id="heading'.$post['post_number'].'">
-                                <h4 class="panel-title">
-                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse'.$post['post_number'].'" aria-expanded="true" aria-controls="collapseOne">
-                                <p class="post post-number01">
-                                    <span class="post-number">'.$post['post_number'].'</span>
-                                    <span class="post-name">'.$post['title'].'</span>
-                                    <span class="post-upload-date">'.$post['date'].'</span>
-                                    <span class="post-like">
-                                        <strong>'.$post['agree_count'].'</strong>
-                                    </span>
-                                </p>
-                                </a>
-                            </h4>
-                                </div>
-                                <div id="collapse'.$post['post_number'].'" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-                                <div class="panel-body">
-                                    <p class="post-text">'.$post['description'].'</p>
-                                    <button type="button" class="btn btn-primary vote-agree">청원동의</button>
-                                    <span class="post-report">Report</span>
-                                </div>
-                                </div>
-                            </div>
-                            ';
-                          }
-                          ?>
+					<div class="vote-thisweek">
+						<div class="row page-title-column">
+							<p class="page-title">5월 첫째주 청원</p><!--<div class="custom-icon">?</div>-->
+							<a href="#" class="custom-icon" data-toggle="tooltip" data-placement="bottom" title="이주의 청원은 매주 월~금요일 동안 가장 많은 청원동의를 받은 글이 채택되며,
+							토~일요일까지 유지된다.">?</a>
 						</div>
-					  </div>
-                    </div>
-			<span class="school-info"><strong>서울아이티고등학교</strong><img src="asset/img/highschool-logo1.png" width="25" height="22" class="img-setting01"></span>
-        </div>
+						<p class="post-info"><span class="post-title"><strong>테스트 제목</strong></span><span class="post-date">2012-12-12</span></p>
+						<p class="post-agree">청원추천 수 <span class="post-agree-count"><strong>20</strong></span></p>
+						<div class="post-description">테스트 본문</div>
+						
+						<div class="input-group mb-3 comment">
+						  <select class="custom-select comment-select" id="inputGroupSelect01">
+							<option selected>Choose...</option>
+							<option value="1">천세진 선생님</option>
+							<option value="2">정병남 선생님</option>
+							<option value="3">이영중 선생님</option>
+						  </select>
+						  <input type="text" class="form-control" placeholder="   댓글을 이곳에 작성하세요." aria-label="Recipient's username" aria-describedby="basic-addon2">
+						  <div class="input-group-append">
+							<button type="submit" class="btn btn-primary comment-submit">전송</button>
+						  </div>
+						</div>
+						
+						<div class="comment-box">
+							<p class="comments">
+							<span class="comment-name"><strong>천세진 선생님</strong>
+							<em class="material-icons icon-check">check_circle</em></span>&nbsp;&nbsp;<span class="comment-description">댓글 테스트</span>
+							</p>
+						</div>
+						
+						
+					</div>
+					<span class="school-info"><strong>서울아이티고등학교</strong><img src="asset/img/highschool-logo1.png" width="25" height="22" class="img-setting01"></span>
+            	</div>
+        	</div>
     </div>
+	<script>
+		$(function () {
+		  $('[data-toggle="tooltip"]').tooltip()
+		})
+	</script>
 </body>
 </html>
--- writing_process.php
+++ writing_process.php
@@ -4,22 +4,23 @@ $conn = mysqli_connect(
   'root',
   '1234',
   'voteservice');
-$today = date("Ymd");
-// id 자동 상승을 해야만 id값을 넣어서 dashboard가 정상적으로 작동됨
+$today = date("Y-m-d");
+$descrition = nl2br($_POST['description']);
+// n12br함수를 이용해서 엔터값을 </br>으로 변환해준다
 $sql = "
   INSERT INTO post
-    (title, description, date, agree_count)
+    (post_number, title, description, date, agree_count)
     VALUES(
+        '',
         '{$_POST['title']}',
-        '{$_POST['description']}',
+        '$descrition',
         '$today',
-        '0',
-        NOW()
+        '0'
     )
 ";
 $result = mysqli_query($conn, $sql);
 if($result === false){
-  echo '<script>alert('저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요')</script>';
+  echo '<script>alert("저장 하는 과정에서 문제가 생겼습니다, 관리자에게 문의해 주세요.")</script>';
   error_log(mysqli_error($conn));
 } else {
   echo("<script>location.replace('dashboard.php');</script>"); 
